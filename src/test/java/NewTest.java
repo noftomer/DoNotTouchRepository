33e52652ec19203b6b508d9373405ed517f2b60d
tomer <tomnof81@gmail.com>
2018-12-07 11:43:08 +0200

    Donot touch

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver_original.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		System.out.println("Succesed");
+		Thread.sleep(1500);
+		driver.quit();
+  }
+}
