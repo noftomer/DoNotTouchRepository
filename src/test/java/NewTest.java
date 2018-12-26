5b8d601bcc1d5be569860be77624986c2fba32dd
tomer <tomnof81@gmail.com>
2018-12-26 19:28:41 +0200

    travis

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class NewTest {
   @Test
   public void f() throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver_original.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDrivers\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.google.com/");
 		System.out.println("Succesed");
